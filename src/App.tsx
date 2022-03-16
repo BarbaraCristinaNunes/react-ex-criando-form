4ff2784de65c7c341f1654d80a7df55e61703b32
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-16 09:30:30 +0100

    I can get input's value and show it on browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
+import React, { useState } from "react";
+
 const App = () => {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [age, setAge] = useState("");
+
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+  const changeSurname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSurname(event.target.value);
+  }
+
+  const changeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(event.target.value);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={changeName}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={surname} onChange={changeSurname}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={age} onChange={changeAge}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {surname}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
